791405350ea6e513b71e2c40638f2d8f0f28cd0f
Vaibhav T Gaikwad <103184302+VaibhavGaikwad03@users.noreply.github.com>
2022-07-24 15:47:08 +0530

    Add files via upload

---
+++ LinkedList.h
@@ -0,0 +1,44 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct node
+{
+    int data;
+    struct node *next;
+};
+
+typedef struct node NODE;
+typedef struct node * PNODE;
+typedef struct node ** PPNODE;
+
+void Display(PNODE Head)
+{
+    printf("Elements from linked list are : \n");
+
+    while(Head != NULL)
+    {
+        printf("|%d| ->",Head ->data);
+        Head = Head -> next;
+    }
+    printf("NULL\n\n");
+}
+
+void InsertFirst(PPNODE head, int no)
+{
+    PNODE newn = NULL;
+
+    newn = (PNODE)malloc(sizeof(NODE));
+
+    newn -> data = no;
+    newn -> next = NULL;
+
+    if(*head == NULL)
+    {
+        *head = newn;
+    }
+    else
+    {
+        newn -> next = *head;
+        *head = newn;
+    }
+}
